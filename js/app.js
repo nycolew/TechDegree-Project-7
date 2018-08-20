540731825acbfa85e6cc940c566ff00b9c5c273d
Nycole <nycwalsh@gmail.com>
2018-08-20 14:47:05 -0500

    Finished project
    Only thing I still want to change is that the words get broken up. Not sure if that can be fixed.

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const keys = document.querySelectorAll('.keyrow button');
 const phrase = document.getElementById('phrase');
 let missed = 0;
 const gameStart = document.getElementsByClassName('btn__reset')[0];
-const title = document.getElementsByClassName('title');
+const title = document.getElementsByClassName('title')[0];
 
 
 // Change cursor to pointer over start button
@@ -22,7 +22,9 @@ const phrases = [
   'A word after a word after a word is power',
   'It does not do to dwell on dreams and forget to live',
   'To love is to act',
-  'Somewhere something incredible is waiting to be known'
+  'Somewhere something incredible is waiting to be known',
+  'Time for some thrilling heroics',
+  'The trouble is you think you have time'
 ];
 
 // Get random phrase and split into array of characters
@@ -35,7 +37,7 @@ function getRandomPhraseAsArray(array) {
 
 // Set phrase letters array to global variable
 
-const phraseLetters = getRandomPhraseAsArray(phrases);
+let phraseLetters = getRandomPhraseAsArray(phrases);
 
 // Add phrase to the game board
 
@@ -89,10 +91,10 @@ function results(className, message) {
 
 function checkWin() {
   const lettersGuessed = document.getElementsByClassName('show');
-  const totalLetters = document.getElementsByClassName('letters');
+  const totalLetters = document.getElementsByClassName('letter');
   const overlay = document.getElementById('overlay');
   if (lettersGuessed.length === totalLetters.length && lettersGuessed.length !== 0) {
-    results("win", "Congratulations! You're going to Disney World!");
+    results("win", "Congratulations! You're a winner!");
   }
   else if (missed >= 5) {
     results("lose", "You lose! Better luck next time!");
@@ -118,3 +120,35 @@ for (let i = 0; i < keys.length; i +=1) {
     checkWin();
   })
 }
+
+// Function to reset phrase
+function newPhrase() {
+  let letters = document.getElementsByClassName('letter');
+  let spaces = document.getElementsByClassName('space');
+  for (let i = letters.length - 1; i >= 0; i--) {
+    letters[i].parentNode.removeChild(letters[i]);
+  }
+  for (let i = spaces.length - 1; i >=0; i--) {
+    spaces[i].parentNode.removeChild(spaces[i]);
+  }
+  phraseLetters = getRandomPhraseAsArray(phrases);
+  addPhraseToDisplay(phraseLetters);
+}
+
+// Reset game
+
+gameStart.addEventListener('click', (e) => {
+  // Reset missed to zero
+  missed = 0;
+  // Reset scoreboard
+  for (let i = 0; i < 5; i += 1) {
+    scoreboard.firstElementChild.children[i].firstElementChild.src = "images/liveHeart.png";
+  }
+  // Reset phrase & generate new phrase
+  newPhrase();
+  // Clear keyboard
+  for (i = 0; i < keys.length; i +=1) {
+    keys[i].removeAttribute('class');
+    keys[i].removeAttribute('disabled');
+  }
+})
